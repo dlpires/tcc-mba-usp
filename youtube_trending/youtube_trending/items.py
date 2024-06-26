d27d32d9c1040fa60b3168d5e38dc5f1199fef33
Diego Luis Pires <dlpires@outlook.com>
2024-06-25 21:57:02 -0300

    1º: Adding kaggle datasets and scrapy youtube trending project

---
+++ youtube_trending/youtube_trending/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class YoutubeTrendingItem(scrapy.Item):
+    # define the fields for your item here like:
+    video_name = scrapy.Field()
+    # video_channel_name = scrapy.Field()
+    # views = scrapy.Field()
+    # upload_time = scrapy.Field()
---
+++ youtube_trending/youtube_trending/spiders/trending.py
@@ -0,0 +1,75 @@
+from scrapy import Spider
+from scrapy.loader import ItemLoader
+import os
+import glob
+import json
+
+# SELENIUM
+from time import sleep
+from typing import Any, Iterable
+from scrapy.selector import Selector
+from scrapy.http import Request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+##
+from youtube_trending.items import YoutubeTrendingItem
+
+
+class TrendingSpider(Spider):
+    name = "trending"
+    allowed_domains = ["www.youtube.com","youtube.com"]
+    start_urls = ["https://www.youtube.com/feed/trending"]
+    
+    def start_requests(self) -> Iterable[Request]:
+        self.driver = webdriver.Firefox()
+        self.driver.get(self.start_urls[0])
+
+        sel = Selector(text=self.driver.page_source)
+        video_name = sel.xpath('//yt-formatted-string/text()').extract()
+        
+        
+
+        # for book in books:
+        #     url = 'https://books.toscrape.com/'+book
+        #     yield Request(url, callback=self.parse_book)
+        
+        # while True:
+        #     try:
+        #         next_page = self.driver.find_element(By.XPATH, '//a[text()="next"]')
+        #         sleep(3)
+        #         self.logger.info('Sleeping for 3 seconds.')
+        #         next_page.click()
+
+        #         sel = Selector(text=self.driver.page_source)
+        #         books = sel.xpath('//h3/a/@href').extract()
+                
+        #         for book in books:
+        #             url = 'https://books.toscrape.com/'+book
+        #             yield Request(url, callback=self.parse_book)
+        #     except NoSuchElementException:
+        #         self.logger.info('No more pages to load.')
+        #         self.driver.quit()
+        #         break
+
+    def parse(self, response):
+        l = ItemLoader(item=YoutubeTrendingItem(), response=response)
+
+        # video_name = response.xpath('//yt-formatted-string/text()').extract()
+        #video_channel_name = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #views = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #upload_time = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+        l.add_value('video_name', video_name)
+        #l.add_value('video_channel_name', video_channel_name)
+        #l.add_value('views', views)
+        #l.add_value('upload_time', upload_time)
+
+
+        yield l.load_item()
+        
+
+    def close(self, reason):
+        json_file = max(glob.iglob('*.json'), key=os.path.getctime)
+        os.rename(json_file, "trendings.json")
