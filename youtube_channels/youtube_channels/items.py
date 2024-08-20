88d4cbee09e9fc74dbe8412b8dfd0fd082665626
Diego Luis Pires <dlpires@outlook.com>
2024-08-20 20:38:34 -0300

    6º: v2 working --> Adjusting youtube channels urls and access some channels info

--- youtube_channels/youtube_channels/items.py
+++ youtube_channels/youtube_channels/items.py
@@ -8,5 +8,4 @@ import scrapy
 
 class YoutubeChannelsItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    channel_name = scrapy.Field()
--- youtube_channels/youtube_channels/spiders/google.py
+++ youtube_channels/youtube_channels/spiders/google.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.loader import ItemLoader
 import json
 
 ## SELENIUM
@@ -11,10 +12,11 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 from time import sleep
 
+from youtube_channels.items import YoutubeChannelsItem
 
 class GoogleSpider(scrapy.Spider):
     name = "google"
-    allowed_domains = ["www.google.com", "google.com"]
+    allowed_domains = ["www.google.com", "google.com", "www.youtube.com", "youtube.com"]
     start_urls = ["https://www.google.com"]
 
 
@@ -23,8 +25,8 @@ class GoogleSpider(scrapy.Spider):
         remote_server = "http://localhost:4444"
         options = webdriver.FirefoxOptions()
         options.set_preference('intl.accept_languages', 'pt-BR, pt')
-        search_key = 'Biologia'
-        num_results = 40
+        search_key = 'Corinthians'
+        num_results = 50
 
         try:
             #self.driver = webdriver.Firefox(options=options)
@@ -55,17 +57,56 @@ class GoogleSpider(scrapy.Spider):
             ## GET URLS LIST
             channels = sel.xpath('//a[@jsname="UWckNb"]/@href').extract()
 
-            print(json.dumps(channels, indent = 4))
+            channels_fmt = []
 
-            yield "success!"
+            ## ADJUSTING LINKS TO ACCESS VIDEOS TAB ON CHANNELS
+            for ch in channels:
+                ## strings in lowercase
+                ch = ch.lower()
 
-        except TimeoutException as timeouterr:
-            self.logger.info('TimeoutException: ', timeouterr)
-        except WebDriverException as webdrivererr:
-            self.logger.info('WebDriverException: ', webdrivererr)
+                ## IF /VIDEOS are included, it's ready!
+                if '/videos' not in ch:
+                    ## CHECK LAST SUBSTRING IN STRING, IF IS THE CHANNEL NAME, INCLUDING /VIDEOS IN LINK 
+                    if '@' in ch.split('/')[-1]:
+                        result = ch+'/videos'
+                    else:
+                        result = ch.replace('/'+ch.split('/')[-1],'/videos')
+                else:
+                    result = ch
+                
+                ## IF ALREADY EXISTS, NOT INCLUDING IN ARRAY
+                if [s for s in channels_fmt if ch.split('/')[3] in s] == []:
+                    channels_fmt.append(result)
+
+            # ACCESS CHANNELS TO PARSE
+            for ch_url in channels_fmt:
+                yield Request(ch_url, callback=self.parse_channels)
+
+        except TimeoutException as te:
+            self.logger.info('TimeoutException: ', te)
+        except WebDriverException as we:
+            self.logger.info('WebDriverException: ', we)
         finally:
             ## CLOSE WEB BROWSER
             self.driver.quit()
+
+
+    ## PARSE CHANNELS
+    def parse_channels(self, response):
+        l = ItemLoader(item=YoutubeChannelsItem(), response=response)
+
+        ## NAVIGATE TO URL
+        self.driver.get(response.request.url)
+        self.logger.info('Sleeping for 3 seconds.')
+        sleep(3)
+
+        sel = Selector(text=self.driver.page_source)
+
+        channel_name = sel.xpath('//yt-dynamic-text-view-model/h1/span/text()').extract_first()
+
+        l.add_value('channel_name', channel_name)
+
+        yield l.load_item()
 
     def parse(self, response):
         pass
